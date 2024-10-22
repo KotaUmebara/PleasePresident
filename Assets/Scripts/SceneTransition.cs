a1021cbe65c0a83eb315ba947178dbe305a292b1
kota-umebara <s2223079@cco.kanagawa-it.ac.jp>
2024-10-22 11:07:16 +0900

    名前入力試作

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -5,19 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnClickStart()
+    {
+        SceneManager.LoadScene("NameScene");
+    }
+
+    public void OnClickNameDecision()
     {
         SceneManager.LoadScene("GameScene");
     }
